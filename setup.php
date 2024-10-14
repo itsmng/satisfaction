f1887fa5d750ab011378585ab48ac72193f345fa
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-10-14 10:43:23 +0200

    bump version

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@
  * Init the hooks of the plugins -Needed
  */
 
-define ("PLUGIN_SATISFACTION_VERSION", "1.5.4");
+define ("PLUGIN_SATISFACTION_VERSION", "2.0.0");
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_SATISFACTION_MIN_GLPI', '9.5');
